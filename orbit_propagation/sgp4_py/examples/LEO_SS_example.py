24c3ec9fa502dc5e9d272c4fe6f675c125b26647
Tunde Demuren <tdemuren15@gmail.com>
2019-10-07 16:28:22 -0700

    Added test notebook using sgp4 orbit prop with pyIGRF

--- orbit_propagation/sgp4_py/examples/LEO_SS_example.py
+++ orbit_propagation/sgp4_py/examples/LEO_SS_example.py
@@ -1,27 +1,33 @@
 from sgp4.earth_gravity import wgs72
 from sgp4.io import twoline2rv
-
+import pyIGRF
+import pyproj
 '''
-Function that loads in satellite object and returns orbit
+Function that loads in satellite object and returns propagated orbit
 '''
 
 # Orsted satellite TLE (https://www.calsky.com/observer/tle.cgi?satid=99008B&tdt=2456641.33063657)
 line1 = ('1 25635U 99008B   13348.59627062  .00000650  00000-0  16622-3 0  9860')
 line2 = ('2 25635 096.4421 173.2395 0141189 010.0389 029.8678 14.46831495780970')
 
-def orbit_prop(TLE, epoch):
-    # parse TLE
-    line1 = TLE[0]
-    line2 = TLE[1]
-    # load in spacecraft object
-    satellite = twoline2rv(line1, line2, wgs72)
-    # propagte orbit based on epoch
-    year = epoch[0]
-    day = epoch[1]
-    month = epoch[2]
-    hour = epoch[3]
-    minute = epoch[4]
-    second = epoch[5]
-    x, v = satellite.propagate(year, month, day, hour, minute, second)
+# load in spacecraft object
+satellite = twoline2rv(line1, line2, wgs72)
+# propagte orbit based on epoch
+year = 2013
+day = 14
+month = 12
+hour = 14
+minute = 18
+second = 37
+r, v = satellite.propagate(year, month, day, hour, minute, second)
+x = r[0]
+y = r[1]
+z = r[2]
+# Convert ECEF position to geodetic
+ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+lon, lat, alt = pyproj.transform(ecef, lla, x, y, z, radians=False)
+# Calculate magnetic field properties
+(D,I,H,Bx,By,Bz,F) = pyIGRF.igrf_variation(lat, lon, alt, year)
 
-    return x,v
+    
