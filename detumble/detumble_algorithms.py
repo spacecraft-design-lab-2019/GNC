6e14c520bed84df5ec1203bd1da6b37f39464a9c
Paul DeTrempe <detrempe@stanford.edu>
2019-10-08 20:01:44 -0700

    Created Python detumble algorithms.

---
+++ detumble/detumble_algorithms.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 19:36:55 2019
+
+@author: Paul DeTrempe
+
+@description: Implementation of magnetorquer detumbling laws (See Wertz 7.5)
+"""
+import numpy as np
+
+def detumble_avanzini(omega,B,k=1):
+    '''
+    Takes in the angular rate and magnetic field vectors
+    (as 3x1 np arrays) and returns a 3x1 vector of control torque to detumble.
+
+    TODO: find optimal k for our system
+    '''
+    b = B/np.linalg.norm(B) # normalize magnetic field vector
+    L = -k*np.matmul((np.identity(3) - np.matmul(b,np.transpose(b))),omega)
+    return L
+    
+def detumble_B_dot(B,B_dot,k=1):
+    '''
+    Takes in magnetic field, magnetic field rate (as 3x1 vectors), and control gain (scalar)
+    and returns a 3x1 control moment
+    '''
+    m = -k*B_dot/np.linalg.norm(B)
+    return m
+    
+    
