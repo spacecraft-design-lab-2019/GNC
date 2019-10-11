20a21fbafd49a12cdefaa7a6f543826161819f00
ethanleboeuf <ethanl20@vt.edu>
2019-10-11 14:48:14 -0700

    Updated folders to split cpp and python

--- util_funcs/__init__.py
+++ util_funcs/__init__.py
@@ -1,3 +1,1 @@
-from .time_functions import date2MJD, MJD2GMST
-from .frame_conversions import eci2ecef, ecef2enu, ecef2lla
-from .sun_utils import sun_position, sat_sun_vect
+import GNC.util_funcs.py_funcs
