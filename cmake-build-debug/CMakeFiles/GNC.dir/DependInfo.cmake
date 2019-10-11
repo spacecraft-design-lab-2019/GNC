a469c82ec03340e97536f13bbe29eca07aa986bf
ethanleboeuf <ethanl20@vt.edu>
2019-10-11 13:56:50 -0700

    Adding in initial C++ stuff. Organizing stuff into Python folder as well.

---
+++ cmake-build-debug/CMakeFiles/GNC.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Ethan/CLionProjects/GNC/util_funcs/cpp/time_functions.cpp" "C:/Users/Ethan/CLionProjects/GNC/cmake-build-debug/CMakeFiles/GNC.dir/util_funcs/cpp/time_functions.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../orbit_propagation/cpp"
+  "../util_funcs/cpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
