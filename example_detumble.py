02d87aa746293b29aa77710e27b9134180a32a68
Paul DeTrempe <detrempe@stanford.edu>
2019-10-18 11:36:52 -0700

    Deleted eigen and pybind test folders. Fixed orbit propagation tests. Added detumble tests.

--- example_detumble.py
+++ example_detumble.py
@@ -5,13 +5,18 @@ Created on Tue Oct  8 20:08:34 2019
 @author: pdetr
 """
 import numpy as np
-from detumble import detumble_avanzini
-
+from detumble.py_funcs import detumble_B_cross,detumble_B_dot,get_B_dot
+from math import sqrt
 
 # Example call of -omega detumble
-omega = np.array([0.0,0.0,1.0])
-B = np.array([0.0,1.0,0.0])
+omega = np.array([0.0, 0.0, 1.0])
+B_1 = np.array([0.0, 1.0, 0.0])
+B_2 = np.array([-sqrt(2), sqrt(2), 0.0])
+dt = .1 # seconds
 
-L = detumble_avanzini(omega,B)
+L_1 = detumble_B_cross(omega,B_2)
+B_dot = get_B_dot(B_1,B_2,dt)
+L_2 = detumble_B_dot(B_2,B_dot)
 
-print(L)
+print(L_1)
+print(L_2)
--- orbit_propagation/test/test_orbit.py
+++ orbit_propagation/test/test_orbit.py
@@ -1,5 +1,5 @@
+from orbit_propagation.orbit_prop_py import orbit
 import numpy as np
-from orbit_prop_py.orbit import *
 
 def test_get_orbit_pos():
     # test epoch
@@ -9,7 +9,7 @@ def test_get_orbit_pos():
     line2 = ('2 25635 096.4421 173.2395 0141189 010.0389 029.8678 14.46831495780970')
     TLE = {'line1': line1, 'line2': line2}
     # Run function
-    np.testing.assert_allclose(test_get_orbit_pos(TLE, epoch), (-5236.501106068836, 1137.472711241124, 4541.50244985641), atol=1e-6)
+    np.testing.assert_allclose(orbit.get_orbit_pos(TLE, epoch), (-5236.501106068836, 1137.472711241124, 4541.50244985641), atol=1e-6)
 
 
 def test_get_orbit_magnetic():
@@ -20,6 +20,5 @@ def test_get_orbit_magnetic():
     line2 = ('2 25635 096.4421 173.2395 0141189 010.0389 029.8678 14.46831495780970')
     TLE = {'line1': line1, 'line2': line2}
     # Run function
-    np.testing.assert_allclose(test_get_orbit_magnetic(TLE, epoch), 
-        (-0.06298895551653533, 0.024098876110463806, 4.691724203241417, 
-        5.228653116191708, -21.512815067758225,29.288854517493746, 26.25341004107369), atol=1e-6)
+    np.testing.assert_allclose(orbit.get_orbit_magnetic(TLE, epoch), 
+        (5.228653116191708, -21.512815067758225, 29.288854517493746), atol=1e-6)
