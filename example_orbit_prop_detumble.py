f2c4c117878444be56ef68a638b0e27c00a0bf45
Paul DeTrempe <detrempe@stanford.edu>
2019-10-18 14:33:47 -0700

    Saving progress on orbit+magnet+detumble example.

--- example_orbit_prop_detumble.py
+++ example_orbit_prop_detumble.py
@@ -21,8 +21,22 @@ Iyy = 100
 Izz = 125
 I = np.array([[Ixx, 0.0, 0.0],[0.0, Iyy, 0.0], [0.0, 0.0, Izz]])
 
-# initial conditions, radians & rad/s
+# initial attitude conditions, radians & rad/s
 q_0 = np.array([[0.0],[0.0],[0.0],[1.0]])                     # initial quaternion, scalar last
 w_0 = np.array([[0.1*pi/180.],[0.1*pi/180.],[pi/180.]])  # initial rotation rate
+
+# initial orbit state conditions, TLE+epoch
+epoch = '2013-12-14T14:18:37.00'
+line1 = ('1 25635U 99008B   13348.59627062  .00000650  00000-0  16622-3 0  9860')
+line2 = ('2 25635 096.4421 173.2395 0141189 010.0389 029.8678 14.46831495780970')
+TLE = {'line1': line1, 'line2': line2}
+
+mean_motion = 14.46/(24*3600)*2*math.pi # mean motion, radians/second
+T = 2*pi/mean_motion                      # Period, seconds
+
+# feed in a vector of times and plot orbit
+t0 = 0
+tf = 0
+times = np.linspace(t0,tf,n)
 
 
