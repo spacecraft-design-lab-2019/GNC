777d3b6649809cd4bf4d6b8e89fd5e03b45b516c
Jayden Zundel <jzundel@stanford.edu>
2019-10-18 21:30:18 -0700

    Predict step of MEKF added

---
+++ MEKF/MEKF.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy import linalg as LA
+import math
+def predict(xk,w,dt):
+    '''
+    Predict step of MEKF - state x = [q, Beta]
+    qk*[cos(theta),rsin(theta)] uses Hamilton's quaternion mult.
+    ** scalar first quaternion ** 
+    '''
+    q = xk[0:4]
+    b = xk[4:7]
+    
+    theta = LA.norm(w-b)*dt
+    r = (w-b)/LA.norm(w-b)
+    
+    xn = np.zeros(7)
+    
+    xn[0] = q[0]*math.cos(theta)-q[1]*r[0]*math.sin(theta)-\
+            q[2]*r[1]*math.sin(theta)-q[3]*r[2]*math.sin(theta)
+    xn[1] = q[0]*r[0]*math.sin(theta)+q[1]*math.cos(theta)\
+            +q[2]*r[2]*math.sin(theta)-q[3]*r[1]*math.sin(theta)
+    xn[2] = q[0]*r[1]*math.sin(theta)-q[1]*r[2]*math.sin(theta)\
+            +q[2]*math.cos(theta)+q[3]*r[0]*math.sin(theta)
+    xn[3] = q[0]*r[2]*math.sin(theta)+q[1]*r[1]*math.sin(theta)\
+            -q[2]*r[0]*math.sin(theta)+q[3]*math.cos(theta)
+    
+    xn[4:7] = b[4:7]
+    
+    
+    return xn, A
+
+
