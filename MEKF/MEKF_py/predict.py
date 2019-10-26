00b50100dc0afe0c6d4ec747e43f1f82e3196a76
Jayden Zundel <jzundel@stanford.edu>
2019-10-26 13:59:21 -0700

    finished predict step with scalar first quaternion

--- MEKF/MEKF_py/predict.py
+++ MEKF/MEKF_py/predict.py
@@ -1,6 +1,14 @@
 import numpy as np
 from numpy import linalg as LA
 import math
+
+def SkewSymmetric(a):
+    a1 = a[0]
+    a2 = a[1]
+    a3 = a[2]
+    a_skew = np.array([[0,-a3,a2],[a3,0,-a1],[-a2,a1,0]])
+    return a_skew
+
 def predict(xk,w,dt):
     '''
     Predict step of MEKF - state x = [q, Beta]
@@ -12,29 +20,46 @@ def predict(xk,w,dt):
     q = xk[0:4]
     b = xk[4:7]
     
-    theta = LA.norm(w-b)*dt
-    r = (w-b)/LA.norm(w-b)
+    u = w + b
+    theta = LA.norm(u-b)*dt
+    r = (u-b)/LA.norm(u-b)
     
     xn = np.zeros(7)
     
-    xn[0] = q[0]*math.cos(theta)-q[1]*r[0]*math.sin(theta)-\
-            q[2]*r[1]*math.sin(theta)-q[3]*r[2]*math.sin(theta)
-    xn[1] = q[0]*r[0]*math.sin(theta)+q[1]*math.cos(theta)\
-            +q[2]*r[2]*math.sin(theta)-q[3]*r[1]*math.sin(theta)
-    xn[2] = q[0]*r[1]*math.sin(theta)-q[1]*r[2]*math.sin(theta)\
-            +q[2]*math.cos(theta)+q[3]*r[0]*math.sin(theta)
-    xn[3] = q[0]*r[2]*math.sin(theta)+q[1]*r[1]*math.sin(theta)\
-            -q[2]*r[0]*math.sin(theta)+q[3]*math.cos(theta)
+    # q2 == s
+    s = np.array([math.cos(theta/2),r*math.sin(theta)])
     
-    xn[4:7] = b[0:3]
+    # propagate dynamics forward
+    xn[0] = q[0]*s[0]-np.transpose(q[1:4])*s[1:4]
+    xn[1:4] = q[0]*s[1:4]+s[0]*q[1:4]+np.cross(q[1:4],s[1:4])
+    xn[4:7] = b
+    
+    # isolate vector component of quaternion
+    V = np.array([[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    
+    # left quaternion multiply
+    Ls = np.zeros(4,4)
+    Ls[0,0] = s[0]
+    Ls[0,1:4] = -np.transpose(s[1:4])
+    Ls[1:4,0] = s[1:4]
+    Ls[1:4,1:4] = s[0]*np.identity(3) + SkewSymmetric(s[1:4])
+    
+    # right quaternion multiply
+    Rs = np.zeros(4,4)
+    Rs[0,0] = s[0]
+    Rs[0,1:4] = -np.transpose(s[1:4])
+    Rs[1:4,0] = s[1:4]
+    Rs[1:4,1:4] = s[0]*np.identity(3) - SkewSymmetric(s[1:4])
+    
     
     A = np.zeros([6,6])
     # fill out A matrix
     for ii in range(3):
-        A[ii,ii] = math.exp(-r[ii]*dt)
-        A[ii,ii+3] = -dt
+        A[ii,ii] = V*np.transpose(Ls)*Rs*np.transpose(V)
+        A[ii,ii+3] = -0.5*dt
         A[ii+3,ii+3] = 1
         
     return xn, A
+
 
 
