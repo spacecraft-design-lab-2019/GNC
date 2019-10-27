443178e06db2d9e74d8d58810af289155a20a429
Jayden Zundel <jzundel@stanford.edu>
2019-10-26 17:00:01 -0700

    error-free predict.py

--- MEKF/MEKF_py/predict.py
+++ MEKF/MEKF_py/predict.py
@@ -17,47 +17,49 @@ def predict(xk,w,dt):
 
 
     '''
-    q = xk[0:4]
-    b = xk[4:7]
-    
+    q = np.reshape(xk[0:4],(4,1))
+    b = np.reshape(xk[4:7],(3,1))
+    w = np.reshape(w,(3,1))
     u = w + b
     theta = LA.norm(u-b)*dt
     r = (u-b)/LA.norm(u-b)
-    
-    xn = np.zeros(7)
+    xn = np.reshape(np.zeros(7),(7,1))
     
     # q2 == s
-    s = np.array([math.cos(theta/2),r*math.sin(theta)])
+    s_scalar = math.cos(theta/2)
+    s_vector = r*math.sin(theta)
+    s_vector = np.reshape(s_vector,(3,1))
+    s = np.vstack((s_scalar,s_vector))
     
     # propagate dynamics forward
-    xn[0] = q[0]*s[0]-np.transpose(q[1:4])*s[1:4]
-    xn[1:4] = q[0]*s[1:4]+s[0]*q[1:4]+np.cross(q[1:4],s[1:4])
+    xn[0] = q[0]*s[0]-np.matmul(q[1:4].T,s[1:4])
+    xn[1:4] = q[0]*s[1:4]+s[0]*q[1:4]+np.reshape(np.cross(q[1:4].T,s[1:4].T),(3,1))
     xn[4:7] = b
     
     # isolate vector component of quaternion
     V = np.array([[0,1,0,0],[0,0,1,0],[0,0,0,1]])
     
     # left quaternion multiply
-    Ls = np.zeros(4,4)
+    Ls = np.zeros([4,4])
     Ls[0,0] = s[0]
-    Ls[0,1:4] = -np.transpose(s[1:4])
-    Ls[1:4,0] = s[1:4]
+    Ls[0,1:4] = -np.reshape(s[1:4],3)
+    Ls[1:4,0] = np.reshape(s[1:4],3)
     Ls[1:4,1:4] = s[0]*np.identity(3) + SkewSymmetric(s[1:4])
     
     # right quaternion multiply
-    Rs = np.zeros(4,4)
+    Rs = np.zeros([4,4])
     Rs[0,0] = s[0]
-    Rs[0,1:4] = -np.transpose(s[1:4])
-    Rs[1:4,0] = s[1:4]
+    Rs[0,1:4] = -np.reshape(s[1:4],3)
+    Rs[1:4,0] = np.reshape(s[1:4],3)
     Rs[1:4,1:4] = s[0]*np.identity(3) - SkewSymmetric(s[1:4])
     
     
     A = np.zeros([6,6])
     # fill out A matrix
-    for ii in range(3):
-        A[ii,ii] = V*np.transpose(Ls)*Rs*np.transpose(V)
-        A[ii,ii+3] = -0.5*dt
-        A[ii+3,ii+3] = 1
+    
+    A[0:3,0:3] = V@Ls.T@Rs@V.T
+    A[0:3,3:6] = -0.5*dt
+    A[3:6,3:6] = 1
         
     return xn, A
 
