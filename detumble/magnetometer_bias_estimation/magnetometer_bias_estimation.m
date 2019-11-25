51209fa044f5810301018310db050da51830e9b8
Paul DeTrempe <pdetrempe@gmail.com>
2019-11-24 23:52:13 -0800

    Added post-processing to Matlab bias estimation.

--- detumble/magnetometer_bias_estimation/magnetometer_bias_estimation.m
+++ detumble/magnetometer_bias_estimation/magnetometer_bias_estimation.m
@@ -7,7 +7,7 @@ bias_mag = 40000;       % [nanoTesla]
 cov = 1000;             % covariance [nanoTesla^2], this number was made up!!!!!!!!!
 scaleF = .02;           % scale factor [??]
 caSense = .02;          % cross-axis sensitivity [??]
-bias_true = 45000 * randn(3,1)        % true bias, [nanoTesla]
+bias_true = 45000 * randn(3,1);        % true bias, [nanoTesla]
 
 [T,scaling_matrix,misalignment_matrix] = get_T_matrix(scaleF,caSense);   % Scaling+Misalignment matrix [-]
 
@@ -18,14 +18,14 @@ B_mag_mean = 45000;
 B_mag_std_dev = 5000;
 for i = 1:N
     % TODO: figure out actual distribution of magnitudes in polar orbit
-    B_true_mag = B_mag_mean; % + 5000*randn();
+    B_true_mag = B_mag_mean + normrnd(0,B_mag_std_dev);
     B_vec = randn(3,1);
     B_true_vec = B_true_mag * B_vec/norm(B_vec);
     B_mat(i,:) = ( measure(B_true_vec,T,bias_true,cov) )';
 end
 
 %% Perform fitting using CVX
-cvx_begin SDP
+cvx_begin SDP quiet
 
 % Primal problem
 variable A(3,3) symmetric
@@ -35,37 +35,31 @@ maximize det_rootn(A)
 
 subject to
 for i = 1:N
-    norm(A*(B_mat(i,:))'-b)<=1;
+    norm(A*(B_mat(i,:))'-b) <= 1;
 end
 
-A>0
+A>0;
 
 cvx_end
 
 A
 b
 
-% find matrix square root for mapping from unit ball
+% find matrix inverse for mapping from unit ball
 [V,D] = eig(A);
 
 A_inv = V*(diag(diag(D).^-1))*V';
 
-
-
-
-%% Perform fitting using linear least squares
-[ center, radii, evecs, v, chi2 ] = ellipsoid_fit(B_mat, '' );
-
-%% Estimate T matrix from each ellipsoid
-[U_sdp,S_sdp,V_sdp] = svd(A_inv)
+%% Solve
+[ center, radii, evecs, v, chi2 ] = ellipsoid_fit( B_mat);
 
 %% Output Error Metrics
-bias_estimated_sdp = A_inv*b
-abs_bias_error_sdp = (bias_estimated_sdp-bias_true)'
+bias_estimated_sdp = A_inv*b;
+abs_bias_error_sdp = (bias_estimated_sdp-bias_true)';
 rel_bias_error_sdp = norm(abs_bias_error_sdp)/norm(bias_true)
 
-bias_estimated_lls = center         % nanoTesla
-abs_bias_error_lls = (bias_estimated_lls-bias_true)'
+bias_estimated_lls = center;       % nanoTesla
+abs_bias_error_lls = (bias_estimated_lls-bias_true)';
 rel_bias_error_lls = norm(abs_bias_error_lls)/norm(bias_true)
 
 %% Manipulate SDP data for plotting
@@ -89,6 +83,31 @@ for i = 1:p
     end
 end
 
+%% Manipulate LLS data for plotting
+[X,Y,Z] = ellipsoid(0,0,0,radii(1),radii(2),radii(3));
+
+q = size(X,1);
+for i = 1:q
+    for j = 1:q
+        point_unrotated = [X(i,j);Y(i,j);Z(i,j)];
+        point_rotated = evecs*point_unrotated;
+        X_rot(i,j) = point_rotated(1)+center(1);
+        Y_rot(i,j) = point_rotated(2)+center(2);
+        Z_rot(i,j) = point_rotated(3)+center(3);
+    end
+end
+
+%% Manipulate true T_matrix data for plotting
+for i = 1:p
+    for j = 1:p
+        point_unsc = [X_unsc(i,j);Y_unsc(i,j);Z_unsc(i,j)];
+        point_sc = B_mag_mean*T*(point_unsc)+bias_true;
+        X_T(i,j) = point_sc(1);
+        Y_T(i,j) = point_sc(2);
+        Z_T(i,j) = point_sc(3);
+    end
+end
+
 %% Plot
 figure;
 % raw data
@@ -100,30 +119,36 @@ xlabel('X, [nT]')
 ylabel('Y, [nT]')
 zlabel('Z, [nT]')
 % least squares fit
-[X,Y,Z] = ellipsoid(center(1),center(2),center(3),radii(1),radii(2),radii(3));
-surf(X,Y,Z,'FaceAlpha',0.5,'EdgeColor','none','FaceColor','b')
+surf(X_rot,Y_rot,Z_rot,'FaceAlpha',0.5,'EdgeColor','none','FaceColor','b')
 % SDP fit
 surf(X_sc,Y_sc,Z_sc,'FaceAlpha',0.4,'EdgeColor','none','FaceColor','c')
+% T matrix scaled up by mean magnitude
+surf(X_T,Y_T,Z_T,'FaceAlpha',.2,'EdgeColor','none','FaceColor','y')
 % Biases
 plot3(0,0,0,'ko','Linewidth',5)
 plot3([0,bias_true(1)],[0,bias_true(2)],[0,bias_true(3)],'r','Linewidth',2)
 plot3([0,bias_estimated_lls(1)],[0,bias_estimated_lls(2)],[0,bias_estimated_lls(3)],'g--','Linewidth',2)
 plot3([0,bias_estimated_sdp(1)],[0,bias_estimated_sdp(2)],[0,bias_estimated_sdp(3)],'k--','Linewidth',2)
-legend('Measurements','Least-squares Fit','Minimum Bounding Ellipsoid','Origin','True bias','Estimated bias (lls)','Estimate bias (sdp)')
+legend('Measurements','Least-squares Fit','Minimum Bounding Ellipsoid','Scaled T matrix','Origin','True bias','Estimated bias (LLS)','Estimated bias (SDP)')
 
+% surf(X_T,Y_T,Z_T,'FaceAlpha',.2,'EdgeColor','none','FaceColor','y')
 
 
 %% Functions
 function [T,scaling_matrix, misalignment_matrix] = get_T_matrix(scaleF,caSense)
-    scaling_matrix = eye(3) + scaleF*diag(randn(3,1));
-    misalignment_matrix = mvnrnd(zeros(1,3),caSense*eye(3),1);
+    scaling_matrix = eye(3) + diag(normrnd(0,scaleF,[3,1]));
+    misalignment_matrix = normrnd(0,caSense,[3,3]);
+    % make skew symmetric
+    misalignment_matrix(2:3,1) = -misalignment_matrix(1,2:3)';
+    misalignment_matrix(3,2) = -misalignment_matrix(2,3);
     misalignment_matrix = misalignment_matrix - diag(diag(misalignment_matrix)); % zero out items on diagonal
     T = scaling_matrix + misalignment_matrix;
 end
 
 function B_measured = measure(B_true,T,bias,covariance)
     multiplicative_noise = T;
-    additive_noise = covariance * randn(3,1);
+    additive_noise = normrnd(0,covariance,[3,1]);
     B_measured = multiplicative_noise*B_true + additive_noise + bias;
 end
+
 
