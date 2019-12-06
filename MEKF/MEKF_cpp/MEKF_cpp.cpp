133f72728d34609239488235a87a2cabfc672158
JaydenZundel <44050642+JaydenZundel@users.noreply.github.com>
2019-12-06 15:24:51 -0800

    Update MEKF_cpp.cpp

--- MEKF/MEKF_cpp/MEKF_cpp.cpp
+++ MEKF/MEKF_cpp/MEKF_cpp.cpp
@@ -3,7 +3,7 @@
 
 using Eigen::MatrixXd;
 
-void MEKF_cpp(MatrixXd &xk, MatrixXd &Pk, MatrixXd w, MatrixXd rB, MatrixXd rN, MatrixXd W, MatrixXd V, double dt) {
+MatrixXd get_xk(MatrixXd &xk, MatrixXd &Pk, MatrixXd w, MatrixXd rB, MatrixXd rN, MatrixXd W, MatrixXd V, double dt) {
 /*
  * xk - state 7x1 (q,Beta)
  * Pk - covariance 6x6
@@ -41,13 +41,37 @@ void MEKF_cpp(MatrixXd &xk, MatrixXd &Pk, MatrixXd w, MatrixXd rB, MatrixXd rN, 
 
         // update step
         update(L,z,xn,Pn,V,C,xk,Pk);
+    return xk;
+}
 
+MatrixXd get_Pk(MatrixXd &xk, MatrixXd &Pk, MatrixXd w, MatrixXd rB, MatrixXd rN, MatrixXd W, MatrixXd V, double dt) {
+ /* same as above, returning Pk instead of xk */
+    // predict step
+    MatrixXd xn(7,1), A(6,6), Pn(6,6);
+    predict(xk,Pk,w,dt,A,xn,Pn);
+
+    // run measurement step
+    MatrixXd y(6,1), R(3,3), C(6,6);
+    measurement(xn(seq(0,3),0),rN,y,R,C);
+
+    // run innovation step to find z
+    MatrixXd z(6,1), S(6,6);
+    Innovation(R, rN, rB, Pn, V, C, z, S);
+
+    // find Kalman Gain
+    MatrixXd L(6,6);
+    L = Pn * C.transpose() * S.inverse();
+
+    // update step
+    update(L,z,xn,Pn,V,C,xk,Pk);
+    return Pk;
 }
 
 
 PYBIND11_MODULE(MEKF_cpp, m) {
 m.doc() = "MEKF propagate step"; // optional module docstring
-m.def("MEKF_cpp", &MEKF_cpp, "Propagate MEKF forward one step");
+m.def("get_xk", &get_xk, "Propagate MEKF forward one step and return xk");
+m.def("get_Pk", &get_Pk, "Propagate MEKF forward one step and return Pk")
 //py::return_value_policy::reference,
 }
 
