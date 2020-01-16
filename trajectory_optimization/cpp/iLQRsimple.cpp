508eacbedb177cf0dcd17111fd44b4eef38ac89e
NickG-NZ <nickgoodson1997@gmail.com>
2020-01-16 11:52:27 -0800

    Finished backward pass implementation

--- trajectory_optimization/cpp/iLQRsimple.cpp
+++ trajectory_optimization/cpp/iLQRsimple.cpp
@@ -52,9 +52,21 @@ void iLQRsimple(dynamicsFunc pendDynPtr,
 	J += Jn(0);
 	Jhist[0] = J;
 
+
+	// Intialize matrices for optimisation
 	MatrixXd S = MatrixXd::Zero(Nx, Nx);
 	MatrixXd s = MatrixXd::Zero(Nx, 1);
 	MatrixXd l = MatrixXd::Constant(Nu, N, 1 + tol);
+
+	MatrixXd Snew = S; // temp matrices for updating
+	MatrixXd snew = s;
+	MatrixXd q(Nx, 1);
+	MatrixXd r(Nu, 1);
+	MatrixXd Ak(Nx, Nx);
+	MatrixXd Bk(Nx, Nu);
+	MatrixXd Kk(Nu, Nx);
+	MatrixXd LH;
+	
 
 	iter = 0;
 	while (l.lpNorm<Infinity>() > tol) {
@@ -67,10 +79,43 @@ void iLQRsimple(dynamicsFunc pendDynPtr,
 		for (int k = (N-1); k > 0; k--) {
 
 			// Calculate cost gradients (for this time step)
-			MatrixXd q = Q * (xtraj(all, k) - xg);
-			MatrixXd r = R * utraj(all, k);
+			q = Q * (xtraj(all, k) - xg);
+			r = R * utraj(all, k);
 
 			// Calculate feed-forward correction and feedback gain
+			// Need to check that these assignments don't slow the algorithm down too much. (Alternative code at botoom of file)
+			Ak = A(all, seq(Nx*(k-1), Nx*k));
+			Bk = B(all, seq(Nu*(k-1), Nu*k));
+			Kk = K(all, seq(Nx*(k-1), Nx*k));
+
+			LH = (R + Bk.transpose()*S*Bk);
+			l(all, k) = LH.colPivHouseholderQr().solve((r + Bk.transpose()*s));
+			K(all, seq(aIdxStrt, aIdxEnd)) = LH.colPivHouseholderQr().solve(Bk.transpose()*S*Ak);
+
+			// Calculate new cost to go matrices (Sk, sk)
+			Snew = Q + Kk.transpose()*R*Kk + (Ak - Bk*Kk).transpose()*S*(Ak - Bk*Kk);
+			snew = q - Kk.transpose()*R + Kk.transpose()*R*l(all, k) + (Ak - Bk*Kk).transpose()*(s - S*Bk*l(all, k));
+			S = Snew;
+			s = snew;
+			
+		}
+	}
+
+
+
+
+}
+
+
+
+void rkstep() {
+
+
+}
+
+/*
+			// This code is less readable than assigning Ak, Bk, Kk as new MatrixXd variables
+			// but indexing should be more compuationally efficient.
 			int bIdxStrt = Nu * k - Nu;
 			int bIdxEnd = Nu * k;
 			int aIdxStrt = Nx * k - Nx;
@@ -81,23 +126,11 @@ void iLQRsimple(dynamicsFunc pendDynPtr,
 			MatrixXd K_RH = (B(all, seq(bIdxStrt, bIdxEnd)).transpose()*S*A(all, seq(aIdxStrt, aIdxEnd)));
 
 			l(all, k) = LH.colPivHouseholderQr().solve(l_RH);  // Use solver to perform inversion
-			K(all, seq(aIdxStrt, aIdxEnd)) = LH.colPivHouseholderQrol().solve(K_RH); // K has same columns as A matrix
+			K(all, seq(aIdxStrt, aIdxEnd)) = LH.colPivHouseholderQr().solve(K_RH); // K has same columns as A matrix
 
 			// Calculate new cost to go matrices (Sk, sk)
-			
+			Snew = Q + K(all, seq(aIdxStrt, aIdxEnd)).transpose()*R*K(all, seq(aIdxStrt, aIdxEnd)) + 
+					(A(all, seq(aIdxStrt, aIdxEnd)) - B(all, seq(bIdxStrt, bIdxEnd))*K(all, seq(aIdxStrt, aIdxEnd).transpose()) *
+					S*((A(all, seq(aIdxStrt, aIdxEnd)) - B(all, seq(bIdxStrt, bIdxEnd))*K(all, seq(aIdxStrt, aIdxEnd));
 
-
-			
-		}
-
-	}
-
-
-
-}
-
-
-void rkstep() {
-
-
-}
+*/
