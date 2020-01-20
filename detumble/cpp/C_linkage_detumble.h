e3b1690206155f8a59d09a201df2c0e29659422e
Paul DeTrempe <pdetrempe@gmail.com>
2020-01-20 11:20:46 -0800

    Added test files for making detumble algorithms callable from C.

---
+++ detumble/cpp/C_linkage_detumble.h
@@ -0,0 +1,11 @@
+/* C_linkage_detumble.h */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void detumble_B_dot(double* omega, double* B, double* max_dipoles, double* commanded_dipole);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
--- detumble/cpp/detumble_algorithms.h
+++ detumble/cpp/detumble_algorithms.h
@@ -2,7 +2,19 @@
 // Created by Paul on 11/5/2019.
 //
 
+#include "../eigen-git-mirror/Eigen/Dense"
+
+using namespace Eigen;
+
 #ifndef GNC_DETUMBLE_ALGORITHMS_H
 #define GNC_DETUMBLE_ALGORITHMS_H
+
+Vector3d detumble_B_cross(Vector3d omega, Vector3d B, double k);
+Vector3d detumble_B_cross_bang_bang(Vector3d omega, Vector3d B, double k, Vector3d max_dipoles);
+Vector3d detumble_B_cross_directional(Vector3d omega, Vector3d B, double k, Vector3d max_dipoles);
+Vector3d detumble_B_dot(Vector3d B, Vector3d B_dot, double k);
+Vector3d detumble_B_dot_bang_bang(Vector3d B_dot, Vector3d max_dipoles);
+Vector3d get_bias_estimate(MatrixXd B_mat);
+Vector3d get_B_dot(Vector3d B1, Vector3d B2, double dt);
 
 #endif //GNC_DETUMBLE_ALGORITHMS_H
