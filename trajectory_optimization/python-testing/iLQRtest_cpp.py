cbf30a6dac23bca1a4c2dbe8163c635b705039c9
NickG-NZ <nickgoodson1997@gmail.com>
2020-01-20 15:09:32 -0800

    Modified CMakeLists to include traj opt

---
+++ trajectory_optimization/python-testing/iLQRtest_cpp.py
@@ -0,0 +1,51 @@
+import pdb
+import os
+import sys
+import inspect
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+currentdir = os.path.dirname(os.path.abspath(filename))
+parentdir = os.path.dirname(currentdir)
+gncdir = os.path.dirname(parentdir)
+
+sys.path.insert(0,parentdir)
+sys.path.insert(0, gncdir)
+
+# import CPP module
+import iLQRsimple_cpp as ilqr
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Test the algorithm
+def main():
+
+	N = 250
+	Nx = 2
+	Nu = 1
+
+	Qf = np.eye(Nx) * 30
+	Q = np.eye(Nx) * 0.01
+	R = 0.3
+
+	x0 = np.zeros((2))
+	xg = np.array([np.pi, 0])
+	utraj0 = np.zeros((Nu, N))
+
+	dt = 0.01
+	tol = 0.001
+
+	# TODO: Replace this with call to C++ version
+	xtraj, utraj, K, Jhist = iLQRsimple_py(x0, xg,utraj0, Q, R, Qf, dt, tol) 
+
+	# Plot results
+	fig, ax = plt.subplots(2, 2, 1)
+	fig.title("iLQR results")
+	ax[0, 0].plot(xtraj[0, :])
+	ax[0, 0].set_title("angle")
+	ax[0, 1].plot(xtraj[1, :])
+	ax[0, 1].set_title("angular rate")
+	ax[1, 0].plot(utraj)
+	ax[1, 0].set_title("Control")
+	ax[1, 1].plot(Jhist)
+
