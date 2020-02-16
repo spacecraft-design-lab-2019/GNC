d6f9e4f440fc42a0246390dd3b946d5c03149ba4
Paul DeTrempe but fun <pdetrempe@gmail.com>
2020-02-15 23:09:04 -0800

    Updated MEKF header file

--- MEKF/MEKF_Cpp/MEKF.hpp
+++ MEKF/MEKF_Cpp/MEKF.hpp
@@ -6,10 +6,15 @@
 #include "../../eigen-git-mirror/Eigen/Dense"
 
 using namespace Eigen;
-void Innovation(MatrixXd R, MatrixXd rN, MatrixXd rB, MatrixXd P, MatrixXd V, MatrixXd C, MatrixXd &z, MatrixXd &S);
-void measurement(MatrixXd q, MatrixXd rN, MatrixXd &y, MatrixXd &R, MatrixXd &C);
-void update(MatrixXd L, MatrixXd z, MatrixXd xn, MatrixXd Pn, MatrixXd V, MatrixXd C, MatrixXd &x, MatrixXd &P);
-void predict(MatrixXd xk, MatrixXd w, double dt, MatrixXd &xn, MatrixXd &A);
+
+MatrixXd predict_xn(MatrixXd xk, MatrixXd Pk, MatrixXd w, double dt, MatrixXd W);
+MatrixXd predict_Pn(MatrixXd xk, MatrixXd Pk, MatrixXd w, double dt, MatrixXd W);
+MatrixXd measurement(MatrixXd q, MatrixXd rN);
+MatrixXd innovation(MatrixXd R, MatrixXd rN, MatrixXd rB);
+MatrixXd update_xk(MatrixXd L, MatrixXd z, MatrixXd xn, MatrixXd Pn, MatrixXd V, MatrixXd C);
+MatrixXd update_Pk(MatrixXd L, MatrixXd z, MatrixXd xn, MatrixXd Pn, MatrixXd V, MatrixXd C);
+
+
 MatrixXd DCM2q(MatrixXd A);
 double trace(MatrixXd A);
 MatrixXd triad_ad(MatrixXd M, MatrixXd V);
