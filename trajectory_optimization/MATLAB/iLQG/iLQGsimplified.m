ad8eaf3cd50bd588218f44c600d428a01943266d
NickG-NZ <nickgoodson1997@gmail.com>
2020-02-19 00:19:43 -0800

    Simplified iLQ code further

--- trajectory_optimization/MATLAB/iLQG/iLQGsimplified.m
+++ trajectory_optimization/MATLAB/iLQG/iLQGsimplified.m
@@ -167,9 +167,8 @@ dcost       = 0;
 z           = 0;
 expected    = 0;
 
-if verbosity > 0
-    fprintf('\n=========== begin iLQG ===========\n');
-end
+
+fprintf('\n=========== begin iLQG ===========\n');
 
 for iter = 1:Op.maxIter 
     
@@ -323,9 +322,6 @@ cnew = permute(cnew, [1 3 2]);
 function [diverge, Vx, Vxx, k, K, dV] = back_pass(cx,cu,cxx,cxu,cuu,fx,fu,fxx,fxu,fuu,lambda,regType,lims,u)
 % Perform the Ricatti-Mayne backward pass
 
-% tensor multiplication for DDP terms
-vectens = @(a,b) permute(sum(bsxfun(@times,a,b),1), [3 2 1]);
-
 N  = size(cx,2);
 n  = numel(cx)/N;
 m  = numel(cu)/N;
@@ -351,34 +347,14 @@ for i = N-1:-1:1
     Qu  = cu(:,i)      + fu(:,:,i)'*Vx(:,i+1);
     Qx  = cx(:,i)      + fx(:,:,i)'*Vx(:,i+1);
     Qux = cxu(:,:,i)'  + fu(:,:,i)'*Vxx(:,:,i+1)*fx(:,:,i);
-    if ~isempty(fxu)
-        fxuVx = vectens(Vx(:,i+1),fxu(:,:,:,i));
-        Qux   = Qux + fxuVx;
-    end
     
     Quu = cuu(:,:,i)   + fu(:,:,i)'*Vxx(:,:,i+1)*fu(:,:,i);
-    if ~isempty(fuu)
-        fuuVx = vectens(Vx(:,i+1),fuu(:,:,:,i));
-        Quu   = Quu + fuuVx;
-    end
-    
     Qxx = cxx(:,:,i)   + fx(:,:,i)'*Vxx(:,:,i+1)*fx(:,:,i);
-    if ~isempty(fxx)
-        Qxx = Qxx + vectens(Vx(:,i+1),fxx(:,:,:,i));
-    end
     
-    Vxx_reg = (Vxx(:,:,i+1) + lambda*eye(n)*(regType == 2));
-    
+    Vxx_reg = (Vxx(:,:,i+1) + lambda*eye(n)*(regType == 2));    
     Qux_reg = cxu(:,:,i)'   + fu(:,:,i)'*Vxx_reg*fx(:,:,i);
-    if ~isempty(fxu)
-        Qux_reg = Qux_reg + fxuVx;
-    end
-    
+
     QuuF = cuu(:,:,i)  + fu(:,:,i)'*Vxx_reg*fu(:,:,i) + lambda*eye(m)*(regType == 1);
-    
-    if ~isempty(fuu)
-        QuuF = QuuF + fuuVx;
-    end
     
     if nargin < 13 || isempty(lims) || lims(1,1) > lims(1,2)
         % no control limits: Cholesky decomposition, check for non-PD
